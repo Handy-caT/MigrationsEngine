7950452b7d75431aa2eebf9affe785398c962a67
Maxim Volkov <37216852+Handy-caT@users.noreply.github.com>
2022-12-31 11:45:33 +0300

    Formatter features added

--- migration/script/command_formatter.py
+++ migration/script/command_formatter.py
@@ -4,6 +4,30 @@ class_with_inner_brackets = re.compile(r'([A-Z][a-zA-Z]*\()(.*)(\))')
 class_without_inner_brackets = re.compile(r'([A-Z][a-zA-Z]*\()([^()]*)(\))')
 
 
+def split_arguments_by_commas(command: str) -> list[str]:
+    parts = command.split(',')
+    parts = [part.strip() for part in parts]
+
+    i = 0
+    while i < len(parts):
+        if parts[i].count('(') > parts[i].count(')'):
+            parts[i] += ', ' + parts[i + 1]
+            parts.pop(i + 1)
+        else:
+            i += 1
+
+    return parts
+
+
+def split_command_by_name_and_brackets(command: str) -> list[str]:
+    matches = re.search(class_with_inner_brackets, command)
+    if matches:
+        return [matches.group(1), matches.group(2), matches.group(3)]
+    else:
+        matches = re.search(class_without_inner_brackets, command)
+        return [matches.group(1), matches.group(2), matches.group(3)]
+
+
 class CommandFormatter:
     @staticmethod
     def _format_command_brackets(command: str) -> list[str]:
@@ -34,4 +58,22 @@ class CommandFormatter:
         if brackets > 2:
             return CommandFormatter._format_command_brackets(command)
         else:
-            return [command]
+            if len(command) > 79:
+                splitted = split_command_by_name_and_brackets(command)
+                result = [splitted[0]]
+                splitted_arguments = split_arguments_by_commas(splitted[1])
+
+                result[-1] += splitted_arguments[0]
+
+                for i in range(1, len(splitted_arguments)):
+                    if len(splitted_arguments[i]) + len(result[-1]) + 2 > 79:
+                        result[-1] += ', '
+                        result.append(splitted_arguments[i])
+                    else:
+                        result[-1] += ', ' + splitted_arguments[i]
+
+                result[-1] += ')'
+
+                return result
+            else:
+                return [command]
--- tests/migration/script/test_command_formatter.py
+++ tests/migration/script/test_command_formatter.py
@@ -1,4 +1,5 @@
-from migration.script.command_formatter import CommandFormatter
+from migration.script.command_formatter import CommandFormatter, split_arguments_by_commas, \
+    split_command_by_name_and_brackets
 
 
 def test_command_formatter_brackets():
@@ -16,3 +17,29 @@ def test_command_formatter_command_with_brackets():
     assert CommandFormatter.format_command(command) == ['AlterColumn("users", "id", ',
                                                         'ColumnNotNull(), ',
                                                         'ColumnDefault("xd"))']
+
+
+def test_command_formatter_command_with_brackets_and_long_name():
+    command = 'AlterColumn("users", "id", "ColumnNotNull", "ColumnDefault", "ShowColumns_users")'
+    assert CommandFormatter.format_command(command) == ['AlterColumn("users", "id", "ColumnNotNull", "ColumnDefault", ',
+                                                        '"ShowColumns_users")']
+
+
+def test_divide_commands_by_commas():
+    command = '"users", "id", 3, "xd"'
+    assert split_arguments_by_commas(command) == ['"users"', '"id"', '3', '"xd"']
+
+    command = '"users", "id", ColumnNotNull(), ColumnDefault("xd")'
+    assert split_arguments_by_commas(command) == ['"users"', '"id"', 'ColumnNotNull()',
+                                                  'ColumnDefault("xd")']
+
+    command = 'AlterColumn("users", "id", ColumnNotNull(), ColumnDefault("xd")), ShowColumns("users")'
+    assert split_arguments_by_commas(command) == ['AlterColumn("users", "id", ColumnNotNull(), ColumnDefault("xd"))',
+                                                  'ShowColumns("users")']
+
+
+def test_split_command_by_name_and_brackets():
+    command = 'AlterColumn("users", "id", ColumnNotNull(), ColumnDefault("xd"))'
+    assert split_command_by_name_and_brackets(command) == ['AlterColumn(',
+                                                           '"users", "id", ColumnNotNull(), '
+                                                           'ColumnDefault("xd")', ')']
